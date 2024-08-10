6be3754901f509eefe27fef220c4afb615cf5003
MohamedMohamed <m01126165016@icloud.com>
2024-08-10 22:08:21 +0300

    Update OutlinedTextField border

--- app/src/main/java/kmp/project/littlelemon/Onboarding.kt
+++ app/src/main/java/kmp/project/littlelemon/Onboarding.kt
@@ -3,6 +3,7 @@ package kmp.project.littlelemon
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -24,9 +25,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -90,9 +93,13 @@ fun Onboarding() {
             OutlinedTextField(
                 value = firstName,
                 onValueChange = { firstName = it },
-                shape = RoundedCornerShape(20),
                 modifier = Modifier
                     .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp))
+                    .border(
+                        BorderStroke(2.dp, Color.LightGray),
+                        RoundedCornerShape(8.dp)
+                    )
             )
 
             Spacer(modifier = Modifier.height(24.dp))
@@ -104,9 +111,13 @@ fun Onboarding() {
             OutlinedTextField(
                 value = lastName,
                 onValueChange = { lastName = it },
-                shape = RoundedCornerShape(20),
                 modifier = Modifier
                     .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp))
+                    .border(
+                        BorderStroke(2.dp, Color.LightGray),
+                        RoundedCornerShape(8.dp)
+                    )
             )
 
             Spacer(modifier = Modifier.height(24.dp))
@@ -119,9 +130,13 @@ fun Onboarding() {
                 value = email,
                 onValueChange = { email = it },
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-                shape = RoundedCornerShape(20),
                 modifier = Modifier
                     .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp))
+                    .border(
+                        BorderStroke(2.dp, Color.LightGray),
+                        RoundedCornerShape(8.dp)
+                    )
             )
 
         }
@@ -141,7 +156,12 @@ fun Onboarding() {
             shape = RoundedCornerShape(25),
             border = BorderStroke(2.dp, Color(0xFFdcae59))
         ) {
-            Text(text = "Register", color = Color.Black, fontWeight = FontWeight.Bold, fontSize = 16.sp)
+            Text(
+                text = "Register",
+                color = Color.Black,
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp
+            )
         }
     }
 }
