b69c6b17fb99c62c42d859631b92afea25ec03b0
MohamedMohamed <m01126165016@icloud.com>
2024-08-10 17:04:56 +0300

    Create Onboarding Screen Composable

---
+++ app/src/main/java/kmp/project/littlelemon/Onboarding.kt
@@ -0,0 +1,141 @@
+package kmp.project.littlelemon
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Onboarding() {
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        Image(
+            painter = (painterResource(id = R.drawable.logo)),
+            contentDescription = "App Logo",
+            modifier = Modifier
+                .width(200.dp)
+                .padding(top = 45.dp, bottom = 20.dp)
+                .align(Alignment.CenterHorizontally),
+            contentScale = ContentScale.FillWidth
+        )
+
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .background(Color(0xFF495E57))
+                .padding(vertical = 50.dp)
+        ) {
+            Text(
+                text = "Let's get to know you",
+                fontSize = 28.sp,
+                color = Color.White,
+                fontWeight = FontWeight.W400,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Text(
+            text = "Personal Information",
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp,
+            modifier = Modifier.padding(15.dp)
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Column(modifier = Modifier.padding(18.dp)) {
+
+            val firstName = remember { mutableStateOf(TextFieldValue()) }
+            val lastName = remember { mutableStateOf(TextFieldValue()) }
+            val email = remember { mutableStateOf(TextFieldValue()) }
+
+            Text(text = "First Name", fontWeight = FontWeight.W500)
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            TextField(
+                value = firstName.value,
+                onValueChange = { firstName.value },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Text(text = "Last Name", fontWeight = FontWeight.W500)
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            TextField(
+                value = lastName.value,
+                onValueChange = { lastName.value },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Text(text = "Email", fontWeight = FontWeight.W500)
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            TextField(
+                value = email.value,
+                onValueChange = { email.value },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(
+                    0xFFF4CE14
+                )
+            ),
+            shape = RoundedCornerShape(25),
+            border = BorderStroke(2.dp, Color(0x2F333333))
+        ) {
+            Text(text = "Register", color = Color.Black, fontWeight = FontWeight.Bold)
+        }
+    }
+}
+
+@Preview(showBackground = true, device = "id:pixel_8_pro")
+@Composable
+fun OnboardingPreview() {
+    Onboarding()
+}
