a1227ce98f61908232d976017d74a5116261d490
MohamedMohamed <m01126165016@icloud.com>
2024-08-12 02:41:49 +0300

    Create Home Screen

---
+++ app/src/main/java/kmp/project/littlelemon/Home.kt
@@ -0,0 +1,25 @@
+package kmp.project.littlelemon
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Home Screen")
+        Button(onClick = { navController.navigate(Profile.route) }) {
+            Text(text = "Profile Screen")
+        }
+    }
+}
