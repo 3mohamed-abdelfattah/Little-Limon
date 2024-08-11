30e35628e63175fea49677f4a684103df27781c2
MohamedMohamed <m01126165016@icloud.com>
2024-08-12 02:43:11 +0300

    Update Onboarding Screen

--- app/src/main/java/kmp/project/littlelemon/Onboarding.kt
+++ app/src/main/java/kmp/project/littlelemon/Onboarding.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -43,9 +42,11 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun Onboarding() {
+fun Onboarding(navController: NavController) {
     Column(modifier = Modifier.fillMaxSize()) {
 
         Image(
@@ -186,7 +187,7 @@ fun Onboarding() {
         Spacer(modifier = Modifier.weight(1f))
 
         Button(
-            onClick = { /*TODO*/ },
+            onClick = { navController.navigate(Home.route) },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(20.dp)
@@ -209,8 +210,9 @@ fun Onboarding() {
     }
 }
 
-@Preview(showBackground = true, device = "id:pixel_8_pro")
+@Preview(showBackground = true)
 @Composable
 fun OnboardingPreview() {
-    Onboarding()
+    val navController = rememberNavController()
+    Onboarding(navController = navController)
 }
