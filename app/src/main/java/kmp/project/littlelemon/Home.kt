f996a1bf57983a15964fde25852eee8fdf7b858e
MohamedMohamed <m01126165016@icloud.com>
2024-08-12 02:55:29 +0300

    ADD Screens Previews

--- app/src/main/java/kmp/project/littlelemon/Home.kt
+++ app/src/main/java/kmp/project/littlelemon/Home.kt
@@ -8,7 +8,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun HomeScreen(navController: NavController) {
@@ -22,4 +24,11 @@ fun HomeScreen(navController: NavController) {
             Text(text = "Profile Screen")
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomePreview() {
+    val navController = rememberNavController()
+    HomeScreen(navController = navController)
 }
--- app/src/main/java/kmp/project/littlelemon/Profile.kt
+++ app/src/main/java/kmp/project/littlelemon/Profile.kt
@@ -8,7 +8,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun ProfileScreen(navController: NavController) {
@@ -22,4 +24,11 @@ fun ProfileScreen(navController: NavController) {
             Text(text = "Logout")
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfilePreview() {
+    val navController = rememberNavController()
+    ProfileScreen(navController = navController)
 }
