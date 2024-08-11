e53166214286df80fb6d2b85350f9e4a6364558b
MohamedMohamed <m01126165016@icloud.com>
2024-08-12 02:42:39 +0300

    implement MyNavigation in MainActivity

--- app/src/main/java/kmp/project/littlelemon/MainActivity.kt
+++ app/src/main/java/kmp/project/littlelemon/MainActivity.kt
@@ -4,13 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Onboarding()
+            MyNavigation()
+        }
+    }
+
+
+    @Composable
+    fun MyNavigation() {
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = Onboarding.route) {
+            composable(Onboarding.route) {
+                Onboarding(navController = navController)
+            }
+            composable(Home.route) {
+                HomeScreen(navController = navController)
+            }
+            composable(Profile.route) {
+                ProfileScreen()
+            }
         }
     }
 }
