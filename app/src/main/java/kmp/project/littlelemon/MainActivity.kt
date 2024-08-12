c4b53ecd385ec9526bbc5237d4e241babcf95377
MohamedMohamed <m01126165016@icloud.com>
2024-08-12 03:05:10 +0300

    Update MainActivity

--- app/src/main/java/kmp/project/littlelemon/MainActivity.kt
+++ app/src/main/java/kmp/project/littlelemon/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,23 +11,6 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyNavigation()
-        }
-    }
-
-
-    @Composable
-    fun MyNavigation() {
-        val navController = rememberNavController()
-        NavHost(navController = navController, startDestination = Onboarding.route) {
-            composable(Onboarding.route) {
-                Onboarding(navController = navController)
-            }
-            composable(Home.route) {
-                HomeScreen(navController = navController)
-            }
-            composable(Profile.route) {
-                ProfileScreen(navController = navController)
-            }
         }
     }
 }
