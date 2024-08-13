24ef07e995f45018e735fe57b8abdb88133e11aa
MohamedMohamed <m01126165016@icloud.com>
2024-08-13 03:25:42 +0300

    Active Logout Button To delete sharedPreferences

--- app/src/main/java/kmp/project/littlelemon/Profile.kt
+++ app/src/main/java/kmp/project/littlelemon/Profile.kt
@@ -1,5 +1,7 @@
 package kmp.project.littlelemon
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,22 +10,37 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun ProfileScreen(navController: NavController) {
+
+    val context = LocalContext.current
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "Profile Screen")
-        Button(onClick = { navController.navigate(Onboarding.route) }) {
+        Button(onClick = {
+            clearSharedPreferences(context)
+            navController.navigate(Onboarding.route)
+        }) {
             Text(text = "Logout")
         }
     }
+}
+
+fun clearSharedPreferences(context: Context) {
+    val sharedPreferences: SharedPreferences =
+        context.getSharedPreferences("Little Lemon", Context.MODE_PRIVATE)
+    val editor = sharedPreferences.edit()
+    editor.clear()
+    editor.apply()
 }
 
 @Preview(showBackground = true)
