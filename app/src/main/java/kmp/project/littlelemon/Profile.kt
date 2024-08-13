06b9b178e25c2d7f5aac7d55f1bb5ed672ea3102
MohamedMohamed <m01126165016@icloud.com>
2024-08-13 04:19:46 +0300

    Finish Profile Screen

--- app/src/main/java/kmp/project/littlelemon/Profile.kt
+++ app/src/main/java/kmp/project/littlelemon/Profile.kt
@@ -2,36 +2,245 @@ package kmp.project.littlelemon
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.sharp.ArrowBack
+import androidx.compose.material.icons.sharp.KeyboardArrowLeft
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun ProfileScreen(navController: NavController) {
 
+    var firstName by remember { mutableStateOf("") }
+    var lastName by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
     val context = LocalContext.current
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Profile Screen")
-        Button(onClick = {
-            clearSharedPreferences(context)
-            navController.navigate(Onboarding.route)
-        }) {
-            Text(text = "Logout")
+    LaunchedEffect(Unit) {
+        val sharedPreferences = context.getSharedPreferences("Little Lemon", Context.MODE_PRIVATE)
+        firstName = sharedPreferences.getString("firstName", "") ?: ""
+        lastName = sharedPreferences.getString("lastName", "") ?: ""
+        email = sharedPreferences.getString("email", "") ?: ""
+    }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+
+            IconButton(onClick = {
+                navController.navigate(Home.route) {
+                    popUpTo("Home") { inclusive = true }
+                }
+            }) { Icon(Icons.Sharp.ArrowBack, contentDescription = "Back") }
+
+            Image(
+                painter = (painterResource(id = R.drawable.logo)),
+                contentDescription = "App Logo",
+                modifier = Modifier
+                    .padding(vertical = 30.dp, horizontal = 50.dp)
+                    .weight(1f),
+                contentScale = ContentScale.FillWidth
+            )
+
         }
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Text(
+            text = "Personal Information",
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp,
+            modifier = Modifier.padding(15.dp)
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Column(modifier = Modifier.padding(18.dp)) {
+
+            Text(text = "First Name", fontWeight = FontWeight.W500, fontSize = 16.sp)
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            OutlinedTextField(
+                value = firstName,
+                onValueChange = { newValue ->
+                    firstName = newValue
+                    saveToSharePreferences(context, "firstName", newValue)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp))
+                    .border(
+                        BorderStroke(2.dp, Color.LightGray),
+                        RoundedCornerShape(8.dp)
+                    ),
+                singleLine = true,
+                trailingIcon = {
+                    if (firstName.isNotEmpty()) {
+                        IconButton(onClick = { firstName = "" }) {
+                            Icon(Icons.Default.Clear, contentDescription = null)
+                        }
+                    }
+                },
+                colors = OutlinedTextFieldDefaults.colors(
+                    cursorColor = Color(0XFF495E57)
+                ),
+                textStyle = TextStyle(fontSize = 18.sp),
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Text(text = "Last Name", fontWeight = FontWeight.W500, fontSize = 16.sp)
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            OutlinedTextField(
+                value = lastName,
+                onValueChange = { newValue ->
+                    lastName = newValue
+                    saveToSharePreferences(context, "lastName", newValue)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp))
+                    .border(
+                        BorderStroke(2.dp, Color.LightGray),
+                        RoundedCornerShape(8.dp)
+                    ),
+                singleLine = true,
+                trailingIcon = {
+                    if (lastName.isNotEmpty()) {
+                        IconButton(onClick = { lastName = "" }) {
+                            Icon(Icons.Default.Clear, contentDescription = null)
+                        }
+                    }
+                },
+                colors = OutlinedTextFieldDefaults.colors(
+                    cursorColor = Color(0XFF495E57)
+                ),
+                textStyle = TextStyle(fontSize = 18.sp),
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Text(text = "Email", fontWeight = FontWeight.W500, fontSize = 16.sp)
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { newValue ->
+                    email = newValue
+                    saveToSharePreferences(context, "email", newValue)
+                },
+                trailingIcon = {
+                    if (email.isNotEmpty()) {
+                        IconButton(onClick = { email = "" }) {
+                            Icon(Icons.Default.Clear, contentDescription = null)
+                        }
+                    }
+                },
+                singleLine = true,
+                colors = OutlinedTextFieldDefaults.colors(
+                    cursorColor = Color(0XFF495E57),
+                ),
+                textStyle = TextStyle(fontSize = 18.sp),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp))
+                    .border(
+                        BorderStroke(2.dp, Color.LightGray),
+                        RoundedCornerShape(8.dp)
+                    )
+            )
+
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Button(
+            onClick = {
+                clearSharedPreferences(context)
+                navController.navigate(Onboarding.route)
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+                .height(50.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(
+                    0xFFF4CE14
+                )
+            ),
+            shape = RoundedCornerShape(25),
+            border = BorderStroke(2.dp, Color(0xFFdcae59))
+        ) {
+            Text(
+                text = "Log Out",
+                color = Color.Black,
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp
+            )
+        }
+    }
+}
+
+fun saveToSharePreferences(context: Context, key: String, value: String) {
+    val sharePreferences: SharedPreferences =
+        context.getSharedPreferences("Little Lemon", Context.MODE_PRIVATE)
+    with(sharePreferences.edit()) {
+        putString(key, value)
+        apply()
     }
 }
 
