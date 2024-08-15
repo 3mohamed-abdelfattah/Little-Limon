350da667f092382a9c4bf35115de759298d50fb7
MohamedMohamed <m01126165016@icloud.com>
2024-08-15 03:30:20 +0300

    Network

---
+++ app/src/main/java/kmp/project/littlelemon/Network.kt
@@ -0,0 +1,24 @@
+package kmp.project.littlelemon
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MenuNetwork(
+    @SerialName("menu")
+    val menu: List<MenuItemNetwork>
+)
+
+@Serializable
+data class MenuItemNetwork(
+    @SerialName("id")
+    val id: Int,
+    @SerialName("title")
+    val title: String,
+    @SerialName("description")
+    val description: String,
+    @SerialName("price")
+    val price: Double,
+    @SerialName("image")
+    val image: String,
+)
