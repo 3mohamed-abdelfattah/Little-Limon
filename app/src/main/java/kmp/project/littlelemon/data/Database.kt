8c2152dc4843ab4952dec96189d6d747e3e7c605
MohamedMohamed <m01126165016@icloud.com>
2024-08-17 04:58:46 +0300

    Organize files

--- app/src/main/java/kmp/project/littlelemon/data/Database.kt
+++ app/src/main/java/kmp/project/littlelemon/data/Database.kt
@@ -1,4 +1,4 @@
-package kmp.project.littlelemon
+package kmp.project.littlelemon.data
 
 import androidx.room.Dao
 import androidx.room.Database
--- app/src/main/java/kmp/project/littlelemon/data/Network.kt
+++ app/src/main/java/kmp/project/littlelemon/data/Network.kt
@@ -1,4 +1,4 @@
-package kmp.project.littlelemon
+package kmp.project.littlelemon.data
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
